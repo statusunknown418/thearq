2d6de167f8ad89982c3eb7436fc84cb8972f3f0a
alvaro <alvarodevcode@outlook.com>
2024-01-23 09:06:10 -0500

    fine I'll start a new project

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,9 @@ import { env } from "~/env";
 export default {
   schema: "./src/server/db/schema.ts",
   driver: "mysql2",
+  out: "./src/server/db",
+  verbose: true,
   dbCredentials: {
     uri: env.DATABASE_URL,
   },
-  tablesFilter: ["mobius_*"],
 } satisfies Config;
