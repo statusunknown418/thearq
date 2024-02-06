4faad44d8bd260317934e54ff18f42bacc6894d2
alvaro <alvarodevcode@outlook.com>
2024-02-06 15:46:59 -0500

    style: nice layout

--- src/app/(authenticated)/workspaces/[slug]/layout.tsx
+++ src/app/(authenticated)/workspaces/[slug]/layout.tsx
@@ -1,11 +1,17 @@
 import { type ReactNode } from "react";
-import { PageNavigation } from "~/components/layout/Navigation";
+import { Sidebar } from "~/components/layout/Sidebar";
 
 export default function AuthenticatedLayout({ children }: { children: ReactNode }) {
   return (
     <section className="relative grid max-h-screen min-h-screen grid-cols-12">
-      <PageNavigation />
-      <section className="col-span-10 max-h-screen overflow-y-scroll">{children}</section>
+      <Sidebar />
+
+      <section className="col-span-10 max-h-screen p-2.5">
+        <div className="h-full overflow-scroll rounded-3xl border border-border/70 bg-secondary-background p-4 shadow-xl">
+          {children}
+        </div>
+      </section>
+
       {/* <nav className="col-span-2 border-l">Secondary collapsible nav</nav> */}
     </section>
   );
