a495a01fe282b0a82977ab38e9fd28c1949f5331
alvaro <alvarodevcode@outlook.com>
2024-03-06 08:04:10 -0500

    fix: fixed cron job

--- src/app/api/lazarus.ts
+++ src/app/api/lazarus.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { db } from "~/server/db";
 import { sessions } from "~/server/db/schema";
 
-export async function POST() {
+export async function GET() {
   await db.insert(sessions).values({
     expires: new Date(),
     sessionToken: "UNUSED",
