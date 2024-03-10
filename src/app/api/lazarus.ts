e91ba0389da5c187e0722e8f1958dcc981d40c6a
alvaro <alvarodevcode@outlook.com>
2024-03-10 17:30:03 -0500

    chore: added redis wake up

--- src/app/api/lazarus.ts
+++ src/app/api/lazarus.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { db } from "~/server/db";
 import { sessions } from "~/server/db/schema";
+import { redis } from "~/server/upstash";
 
 export async function GET() {
   await db.insert(sessions).values({
@@ -8,6 +9,8 @@ export async function GET() {
     sessionToken: "UNUSED",
     userId: "UNUSED",
   });
+
+  await redis.ping();
 
   return NextResponse.json({ message: "CRON executed, db won't die. For now" });
 }
--- src/server/defer/redis-ping.ts
+++
@@ -1,8 +0,0 @@
-import { defer } from "@defer/client";
-import { redis } from "../upstash";
-
-export default defer.cron(async () => {
-  const pong = await redis.ping();
-
-  return pong;
-}, "0 0 * * *");
