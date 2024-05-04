ef1671e3037d8e53249e3967e5e0e4f3536b85d4
alvaro <alvarodevcode@outlook.com>
2024-05-03 21:51:31 -0500

    fix: revert to no-caching, may be interferring with stale data updates

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -1,20 +1,16 @@
 "use client";
 
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
 
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { type AppRouter } from "~/server/api/root";
-import { createIDBPersister } from "./persister";
 import { getUrl, transformer } from "./shared";
 
 export const api = createTRPCReact<AppRouter>();
 
 export function TRPCReactProvider(props: { children: React.ReactNode }) {
-  const persister = createIDBPersister();
-
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -43,17 +39,10 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
   );
 
   return (
-    <PersistQueryClientProvider
-      client={queryClient}
-      persistOptions={{
-        persister,
-        maxAge: 1000 * 60 * 60 * 4,
-        buster: "persisted-indexed-db",
-      }}
-    >
+    <QueryClientProvider client={queryClient}>
       <api.Provider client={trpcClient} queryClient={queryClient}>
         {props.children}
       </api.Provider>
-    </PersistQueryClientProvider>
+    </QueryClientProvider>
   );
 }
